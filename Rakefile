919c38a5d99c6ab1ff89b13efcc09ff454b5a0d7
Justin Balthrop <justin@geni.com>
2010-02-19 18:08:20 -0800

    initial commit

---
+++ Rakefile
@@ -0,0 +1,56 @@
+require 'rubygems'
+require 'rake'
+
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gem|
+    gem.name = "active_node"
+    gem.summary = %Q{Lightweight, restful resource wrapper.}
+    gem.description = %Q{A very thin, restful resource wrapper specifically built with a Jiraph-based graph service in mind.}
+    gem.email = "code@justinbalthrop.com"
+    gem.homepage = "http://github.com/ninjudd/active_node"
+    gem.authors = ["Justin Balthrop"]
+    # gem is a Gem::Specification... see http://www.rubygems.org/read/chapter/20 for additional settings
+  end
+  Jeweler::GemcutterTasks.new
+rescue LoadError
+  puts "Jeweler (or a dependency) not available. Install it with: sudo gem install jeweler"
+end
+
+require 'rake/testtask'
+Rake::TestTask.new(:test) do |test|
+  test.libs << 'lib' << 'test'
+  test.pattern = 'test/**/*_test.rb'
+  test.verbose = true
+end
+
+begin
+  require 'rcov/rcovtask'
+  Rcov::RcovTask.new do |test|
+    test.libs << 'test'
+    test.pattern = 'test/**/*_test.rb'
+    test.verbose = true
+  end
+rescue LoadError
+  task :rcov do
+    abort "RCov is not available. In order to run rcov, you must: sudo gem install spicycode-rcov"
+  end
+end
+
+task :test => :check_dependencies
+
+task :default => :test
+
+require 'rake/rdoctask'
+Rake::RDocTask.new do |rdoc|
+  if File.exist?('VERSION')
+    version = File.read('VERSION')
+  else
+    version = ""
+  end
+
+  rdoc.rdoc_dir = 'rdoc'
+  rdoc.title = "active_node #{version}"
+  rdoc.rdoc_files.include('README*')
+  rdoc.rdoc_files.include('lib/**/*.rb')
+end
---
+++ lib/active_node.rb
@@ -0,0 +1,88 @@
+require 'net/http'
+require 'cgi'
+require 'json'
+
+module ActiveNode
+  METHODS = [:get, :put, :post, :delete]
+
+  class Error   < StandardError; end
+  class ConnectionError < Error; end
+  class TimeoutError    < Error; end
+
+  def self.server(host)
+    @servers ||= {}
+    @servers[host] ||= Net::HTTP.new(*host.split(':'))
+  end
+
+  module ClassMethods
+    def node_type(type = nil)
+      if type
+        @node_type = type
+      else
+        @node_type ||= name.downcase
+      end
+    end
+
+    def new(*args)
+      if args.size == 1 and args.first =~ /^(\w)\/(\d)$/
+        raise "invalid node type" unless $1 == node_type
+        model = respond_to?(:find) ? find($2.to_i) : super()
+        model.instance_variable_set(:@node_id, args.first)
+      else
+        model = super
+        model.instance_variable_set(:@node_id, "#{node_type}:#{model.id}")
+      end
+      model
+    end
+
+    METHODS.each do |method|
+      define_method(method) do |resource, *args|
+        resource = "/#{node_type}/#{resource}" unless resource =~ /^\//
+        put_or_post = [:put, :post].include?(method)
+        args << '' if args.empty? and put_or_post
+
+        begin
+          if args.first.kind_of?(Hash)
+            if put_or_post
+              args.unshift(args.shift.to_json)
+            else
+              resource << "?" << args.shift.collect do |key, val|
+                "#{CGI.escape(key.to_s)}=#{CGI.escape(val.to_s)}"
+              end.join('&')
+            end
+          end
+
+          response = server.send(method, resource, *args)
+          if response.code =~ /\A2\d{2}\z/
+            body = response.body
+            return nil if body.empty? or body == 'null'
+            return JSON.load(body)
+          end
+          raise ActiveNode::Error, "#{method} to http://#{host}#{resource} failed with HTTP #{response.code}"
+        rescue Errno::ECONNREFUSED => e
+          raise ActiveNode::ConnectionError, "connection refused on #{method} to http://#{host}#{resource}"
+        rescue TimeoutError => e
+          raise ActiveNode::ConnectionError, "timeout on #{method} to http://#{host}#{resource}"
+        end
+      end
+    end
+  end
+
+  module InstanceMethods
+    attr_accessor :node_id
+
+    METHODS.each do |method|
+      define_method(method) do |resource, *args|
+        resource = "/#{node_id}/#{resource}" unless resource =~ /^\//
+        self.class.send(method, resource, *args)
+      end
+    end
+  end
+end
+
+class Class
+  def active_node
+    extend  ActiveNode::ClassMethods
+    include ActiveNode::InstanceMethods
+  end
+end
