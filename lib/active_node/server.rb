c1f5034d6b3a4f8d270a2b5ab64bcde1e83da59c
Justin Balthrop <git@justinbalthrop.com>
2010-06-17 12:52:13 -0700

    switch from net/http to typheous

--- lib/active_node/server.rb
+++ lib/active_node/server.rb
@@ -1,5 +1,5 @@
-require 'net/http'
-require 'cgi'
+require 'rubygems'
+require 'typhoeus'
 require 'json'
 
 module ActiveNode
@@ -9,48 +9,42 @@ module ActiveNode
 
     def initialize(host)
       @host = host || DEFAULT_HOST
-      @http = Net::HTTP.start(*@host.split(':'))
     end
 
-    def read(path, opts = nil)
-      path << query_string(opts) if opts
-      http(:get, path)
+    def read(path, params = nil)
+      http(path, :params => params)
     end
 
-    def write(path, data, opts = nil)
-      path << query_string(opts) if opts
-      data = data.to_json
-      http(:post, path, data, 'Content-type' => 'application/json')
+    def write(path, data, params = nil)
+      http(path,
+        :method  => :post,
+        :body    => data.to_json,
+        :params  => params,
+        :headers => 'Content-type' => 'application/json'
+      )
     end
 
   private
 
-    def http(method, *args)
-      response = @http.send(method, *args)
-      if response.code =~ /\A2\d{2}\z/
+    def http(path, opts = {})
+      response = Typhoeus::Request.run("#{host}#{path}", opts.merge(:timeout => 5000))
+      if response.success?
         return parse_body(response.body)
-      elsif response.code =~ /\A4\d{2}\z/
+      elsif response.code == 0
+        if time > 0
+          raise ActiveNode::ConnectionError, "timeout on #{method} to http://#{host}#{path}"
+        else
+          raise ActiveNode::ConnectionError, "connection refused on #{method} to http://#{host}#{path}"
+        end
+      else
         error = parse_body(response.body).pretty_inspect
+        raise ActiveNode::Error, "#{method} to http://#{host}#{args.first} failed with HTTP #{response.code}\n#{error}"
       end
-      raise ActiveNode::Error, "#{method} to http://#{host}#{args.first} failed with HTTP #{response.code}\n#{error}"
-    rescue Errno::ECONNREFUSED => e
-      raise ActiveNode::ConnectionError, "connection refused on #{method} to http://#{host}#{args.first}"
-    rescue TimeoutError => e
-      raise ActiveNode::ConnectionError, "timeout on #{method} to http://#{host}#{args.first}"
     end
 
     def parse_body(body)
       return nil if body.empty? or body == 'null'
       JSON.load(body)
-    end
-
-    def query_string(opts)
-      if opts
-        raise ArgumentError, "opts must be Hash" unless opts.kind_of?(Hash)
-        "?" << opts.collect do |key, val|
-          "#{CGI.escape(key.to_s)}=#{CGI.escape(val.to_s)}"
-        end.join('&')
-      end
     end
   end
 end
