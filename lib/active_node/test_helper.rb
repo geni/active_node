7319f978fd4f18fedabb8bf41f9467bfdb1f4a58
Justin Balthrop <git@justinbalthrop.com>
2010-06-18 17:51:12 -0700

    update test_helper

---
+++ lib/active_node/test_helper.rb
@@ -0,0 +1,38 @@
+module ActiveNode
+  class TestServer
+    attr_reader :requests
+
+    def initialize(*mock_responses)
+      @requests  = []
+      @responses = mock_responses || []
+    end
+
+    def read(resource, params = nil)
+      request(resource, :get, params)
+    end
+
+    def write(resource, data, params = nil)
+      request(resource, :put, params, data)
+    end
+
+  private
+
+    def request(resource, method, params, data = nil)
+      opts = {
+        :body   => data ? data.to_json : nil,
+        :params => params,
+        :method => method,
+      }
+      @requests << Typhoeus::Request.new(resource, opts)
+      @responses.size > 1 ? @responses.shift : @responses.first
+    end
+  end
+
+  module TestHelper
+    def mock_active_node(*responses)
+      server = ActiveNode::TestServer.new(*responses)
+      ActiveNode.stubs(:server).returns(server)
+      server
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,38 +1,6 @@
 require 'rubygems'
 require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/active_node'
+require File.dirname(__FILE__) + '/../lib/active_node/test_helper'
 require 'shoulda'
-require 'ostruct'
 require 'pp'
-
-class TestHTTP
-  attr_reader :requests
-
-  def self.create_response(code='200', body='')
-    OpenStruct.new( :code => code, :body => body)
-  end
-
-  def initialize(mock_responses=nil)
-    @requests  = []
-    @responses = [mock_responses || self.class.create_response].flatten
-  end
-
-  [:get, :post].each do |method|
-    define_method(method) do |resource, *args|
-      (data, headers, extra_args) = args
-      @requests << { :method     => method,
-                     :resource   => resource,
-                     :data       => data,
-                     :headers    => headers,
-                     :extra_args => extra_args }
-
-      @responses.size > 1 ? @responses.shift : @responses.first
-    end
-  end
-end
-
-class TestModel < ActiveNode::Base
-  def self.mock_server(responses=nil)
-    ActiveNode.server(nil, nil).instance_variable_set(:@http, TestHTTP.new(responses))
-  end
-end
