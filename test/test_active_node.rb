43de73c1c5594fea3e23fd3aacc22ce2b2edee03
lance bradley <l@ncebradley.org>
2010-09-16 15:52:39 -0700

    fix tests

--- test/test_active_node.rb
+++ test/test_active_node.rb
@@ -1,22 +1,24 @@
 require File.dirname(__FILE__) + '/test_helper'
 
+class TestModel < ActiveNode::Base; end
+
 class ActiveNodeTest < Test::Unit::TestCase
+  include ActiveNode::TestHelper
+  
   context "An ActiveNode class" do
     should "send attributes as JSON body on write" do
-      server = TestModel.mock_server
-
-      TestModel.write_graph('add', { :name => 'Harley' })
-
-      assert_equal 1, server.requests.size
-      req = server.requests.shift
-
-      assert_equal  :post,                        req[:method]
-      assert_equal  '/test_model/add',            req[:resource]
-      assert_equal( {'name' => 'Harley'}.to_json, req[:data])
-      assert_equal  'application/json',           req[:headers]['Content-type']
+      mock_active_node({ :name => 'Harley'}) do |server|
+        assert_equal 1, server.requests.size
+        req = server.requests.shift
+        
+        assert_equal  :post,                        req[:method]
+        assert_equal  '/test_model/add',            req[:resource]
+        assert_equal( {'name' => 'Harley'}.to_json, req[:data])
+        assert_equal  'application/json',           req[:headers]['Content-type']
+      end
     end
   end
-
+  
   context "An ActiveNode model" do
     should "set @node_id in init" do
       t = TestModel.init('model-43')
@@ -25,55 +27,61 @@ class ActiveNodeTest < Test::Unit::TestCase
 
     should "set cached layer_data in init" do
       t = TestModel.init( 'test_model' => {:name => 'Charlie', 'occupation' => 'chocolate maker'} )
-
+      
       assert_equal [:test_model],     t.layer_data.keys
       assert_equal 'Charlie',         t.layer_data[:test_model][:name]
       assert_equal 'chocolate maker', t.layer_data[:test_model]['occupation']
     end
 
     should "send attributes as JSON body on write" do
-      server = TestModel.mock_server
-      TestModel.init('test_model-1').write_graph('update', { :name => 'Charlie' })
+      mock_active_node do |server|
+        TestModel.init('test_model-1').write_graph('update', { :name => 'Charlie' })
 
-      assert_equal 1, server.requests.size
-      req = server.requests.shift
+        assert_equal 1, server.requests.size
+        req = server.requests.shift
 
-      assert_equal  :post,                         req[:method]
-      assert_equal  '/test_model-1/update',        req[:resource]
-      assert_equal( {'name' => 'Charlie'}.to_json, req[:data])
-      assert_equal  'application/json',            req[:headers]['Content-type']
+        assert_equal  :post,                         req[:method]
+        assert_equal  '/test_model-1/update',        req[:resource]
+        assert_equal( {'name' => 'Charlie'}.to_json, req[:data])
+        assert_equal  'application/json',            req[:headers]['Content-type']
+      end
     end
 
     should "read all attributes when called with no resource" do
-      server = TestModel.mock_server
-      TestModel.init('test_model-1').read_graph('')
+      mock_active_node do |server|
+        TestModel.init('test_model-1').read_graph('')
 
-      assert_equal 1, server.requests.size
-      req = server.requests.shift
+        assert_equal 1, server.requests.size
+        req = server.requests.shift
 
-      assert_equal  :get,             req[:method]
-      assert_equal  '/test_model-1/', req[:resource]
+        assert_equal  :get,             req[:method]
+        assert_equal  '/test_model-1/', req[:resource]
+      end
     end
-
+    
     should "read specified attributes when called with resource" do
-      server = TestModel.mock_server
-      TestModel.init('test_model-1').read_graph('profile')
+      mock_active_node do |server|
+        server = TestModel.mock_server
+        TestModel.init('test_model-1').read_graph('profile')
 
-      assert_equal 1, server.requests.size
-      req = server.requests.shift
+        assert_equal 1, server.requests.size
+        req = server.requests.shift
 
-      assert_equal  :get,                    req[:method]
-      assert_equal  '/test_model-1/profile', req[:resource]
+        assert_equal  :get,                    req[:method]
+        assert_equal  '/test_model-1/profile', req[:resource]
+      end
     end
 
     should 'pass extra params on query string' do
-      server = TestModel.mock_server
-      TestModel.init('test_model-1').write_graph('update', { :name => 'Bob' }, { 'test' => 'testing' })
+      mock_active_node do |server|
+        server = TestModel.mock_server
+        TestModel.init('test_model-1').write_graph('update', { :name => 'Bob' }, { 'test' => 'testing' })
 
-      assert_equal 1, server.requests.size
-      req = server.requests.shift
-
-      assert_equal  '/test_model-1/update?test=testing', req[:resource]
+        assert_equal 1, server.requests.size
+        req = server.requests.shift
+        
+        assert_equal  '/test_model-1/update?test=testing', req[:resource]
+      end
     end
   end
 end
