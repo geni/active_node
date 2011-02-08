df1139313ec6c22f11bb9550c4c35e3e6a0a3472
Mike Stangel <mike@geni.com>
2011-02-08 12:50:07 -0800

    blank path causes invalid trailing slash - use "get" for default path instead

--- lib/active_node.rb
+++ lib/active_node.rb
@@ -119,8 +119,8 @@ private
   end
 
   module InstanceMethods
-    def read_graph(path='', opts = nil)
-      (opts, path) = [path, ''] if path.kind_of?(Hash)
+    def read_graph(path='get', opts = nil)
+      (opts, path) = [path, 'get'] if path.kind_of?(Hash)
       path = ActiveNode.resolve_path(path, node_id)
       self.class.read_graph(path, opts)
     end
