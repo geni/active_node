f0922c24517d5aa18eda76eec2ebe48327d9fbdb
ninjudd <git@justinbalthrop.com>
2011-06-14 13:35:04 -0700

    add missing files

---
+++ lib/active_node/core_ext.rb
@@ -0,0 +1,21 @@
+class String
+  # Add underscore and camelize methods if they aren't there. Copied from ActiveSupport::Inflector
+  def underscore
+    self.gsub(/::/, '/').
+      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      tr("-", "_").downcase
+  end unless instance_methods.include?('underscore')
+
+  def camelize
+    gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+  end unless instance_methods.include?('camelize')
+
+  def constantize
+    constant = Object
+    split('::').each do |name|
+      constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+    end
+    constant
+  end unless instance_methods.include?('constantize')
+end
