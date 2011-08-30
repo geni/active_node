d669ea11792048ec3f80132653fe14e6cc256b19
Justin Balthrop <git@justinbalthrop.com>
2011-08-30 13:39:54 -0700

    utils

---
+++ lib/active_node/utils.rb
@@ -0,0 +1,10 @@
+module ActiveNode::Utils
+  def self.extract_options(args)
+    raise ArgumentError, "wrong number of arguments (#{args.size} for 1)" if args.size > 1
+    args.first || {}
+  end
+
+  def self.try(object, method, *args)
+    object.send(method, *args) if object.respond_to?(method)
+  end
+end
