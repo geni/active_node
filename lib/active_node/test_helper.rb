256e983024bb015d4fa09b5e2fb7b7e5b426ae03
Mike Stangel <mike@geni.com>
2012-01-03 17:29:31 -0800

    fix for mocked test server missing context in paths

--- lib/active_node/test_helper.rb
+++ lib/active_node/test_helper.rb
@@ -8,20 +8,36 @@ module ActiveNode
       @responses = mock_responses || []
     end
 
+    def set_host!(host)
+      @host = host
+      self
+    end
+
   private
 
     def http(opts)
       @requests << opts.merge(:method => opts[:method],
-                              :path   => opts[:path],
+                              :path   => path_with_context(opts[:path]),
                               :body   => opts[:data] ? opts[:data].to_json : nil)
       @responses.size > 1 ? @responses.shift : @responses.first
+    end
+
+    def path_with_context(path)
+      (hostname, context) = host.to_s.split(/\//, 2)
+      return path if context.to_s.blank?
+      context += '/' unless path.starts_with?('/')
+      "/#{context}#{path}"
     end
   end
 
   module TestHelper
     def mock_active_node(*responses)
       server = ActiveNode::TestServer.new(*responses)
-      ActiveNode.stubs(:server).returns(server)
+
+      ActiveNode::Server.stubs(:init).with do |host|
+        server.set_host!(host)
+      end.returns(server)
+
       yield(server) if block_given?
       server
     end
@@ -31,7 +47,6 @@ module ActiveNode
         stubs(:write).raises(e)
         stubs(:read).raises(e)
       end
-      ActiveNode.stubs(:server).returns(server)
       server
     end
   end
